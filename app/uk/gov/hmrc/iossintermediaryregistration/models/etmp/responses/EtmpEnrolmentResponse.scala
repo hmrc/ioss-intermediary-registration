35979a317c239c70304be9faa150b4cd3b3e92f3
Andrew Smith <1895818+DevAndyroo@users.noreply.github.com>
2025-09-11 10:51:18 +0100

    VEI-502 ensure enrolment response model updated (#20)

--- app/uk/gov/hmrc/iossintermediaryregistration/models/etmp/responses/EtmpEnrolmentResponse.scala
+++ app/uk/gov/hmrc/iossintermediaryregistration/models/etmp/responses/EtmpEnrolmentResponse.scala
@@ -24,7 +24,7 @@ case class EtmpEnrolmentResponse(
                                   processingDateTime: LocalDateTime,
                                   formBundleNumber: String,
                                   vrn: String,
-                                  intermediary: String,
+                                  intRef: String,
                                   businessPartner: String
                                 )
 
