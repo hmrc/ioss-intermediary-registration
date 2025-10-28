41a90eea72bf8169ba184a5e4bfb5c09e3643a0f
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-10-28 12:12:14 +0000

    add getAccounts to RegistrationController

--- app/uk/gov/hmrc/iossintermediaryregistration/controllers/RegistrationController.scala
+++ app/uk/gov/hmrc/iossintermediaryregistration/controllers/RegistrationController.scala
@@ -183,4 +183,12 @@ case class RegistrationController @Inject()(
           InternalServerError(errorMessage).toFuture
       }
   }
+
+  def getAccounts: Action[AnyContent] = cc.authAndRequireIoss().async {
+    implicit request =>
+      enrolmentsConnector.es2(request.credentials.providerId).map {
+        case Right(enrolments) => Ok(Json.toJson(enrolments))
+        case Left(e) => InternalServerError(e.body)
+      }
+  }
 }
